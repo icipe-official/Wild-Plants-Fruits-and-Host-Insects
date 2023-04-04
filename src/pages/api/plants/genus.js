180f71926ecb348871651928d73606351c0c9072
bonfaceonyango <bonfaceb21@gmail.com>
2023-04-04 16:33:15 +0300

    get plant genera

--- src/pages/api/plants/genus.js
+++ src/pages/api/plants/genus.js
@@ -1,4 +1,5 @@
 import prisma from "../../../../lib/prisma";
+import NextCors from "nextjs-cors";
 
 /**
 
@@ -8,12 +9,20 @@ Retrieves plant genera data.
 @returns {Object} The retrieved plant genera data.
 */
 export default async function handle(req, res) {
-if (req.method == "GET") {
-try {
-const genus = await prisma.plant_genera.findMany({});
-return res.json(genus);
-} catch (e) {
-console.error(e);
-}
-}
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  if (req.method == "GET") {
+    try {
+      const genus = await prisma.plant_genera.findMany({});
+      return res.json(genus);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
