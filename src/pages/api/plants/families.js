66d8e69bcdaed52de6c3d7d0617a7746851fe439
bonfaceonyango <bonfaceb21@gmail.com>
2023-04-04 16:32:45 +0300

    get plant families

--- src/pages/api/plants/families.js
+++ src/pages/api/plants/families.js
@@ -1,4 +1,5 @@
 import prisma from "../../../../lib/prisma";
+import NextCors from "nextjs-cors";
 
 /**
  * Retrieves all plant families from the database and returns them sorted alphabetically.
@@ -6,12 +7,20 @@ import prisma from "../../../../lib/prisma";
  * @param res - The response object.
  */
 export default async function handle(req, res) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   if (req.method == "GET") {
     try {
       const families = await prisma.plant_families.findMany({
         orderBy: {
-          family_name: 'asc' // sort by family name in ascending order
-        }
+          family_name: "asc", // sort by family name in ascending order
+        },
       });
 
       return res.json(families);
