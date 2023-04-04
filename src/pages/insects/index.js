8cda7fc5863ec73c27153db84918bb9104e65f7f
bonfaceonyango <bonfaceb21@gmail.com>
2023-04-04 16:28:14 +0300

    insects default page;

--- src/pages/insects/index.js
+++ src/pages/insects/index.js
@@ -1,123 +1,130 @@
-import { Container,Box } from "@mui/material"
-import Order from "../../components/insects/dipteraTephritidae"
-import dynamic from 'next/dynamic';
+import { Container, Box } from "@mui/material";
+import {
+  ButtonBase,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  useMediaQuery,
+  AppBar,
+  CssBaseline,
+  Toolbar,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+
+import { useState } from "react";
+// import Order from "../src/components/insects/dipteraTephritidae";
+import dynamic from "next/dynamic";
+import DipteraTephridiae2Clientside from "components/insects/clientsideDiptera";
+import LepidopteraClientside from "components/insects/lepidoptera";
+import ColeopteraComponent from "components/insects/coleoptera";
+import HymenopteraBraconidaeComponent from "components/insects/hymenopterabraconidae";
+import HymenopterahymenopteraIchneumonoidae from "components/insects/hymenopteraichmonidae";
+import OtherHymenopteracomponent from "components/insects/otherhymenoptera";
+import { Button } from "react-admin";
+import { makeStyles } from "@mui/styles";
+const drawerWidth = 240;
+
 //Dynamic imports to avoid hydration error. ensure serverdide and client side rendering are the same
-const Coleoptera = dynamic(() => import('components/insects/coleoptera'), {
-  ssr: false,
-});
-import DipteraTephritidae from "components/insects/dipteraTephritidae";
-const Lepidoptera = dynamic(() => import('components/insects/lepidoptera'), {
-  ssr: false,
-});
-const DipteraTephridiae = dynamic(() => import('components/insects/dipteraTephritidae'), {
-  ssr: false,});
- 
-  const DipteraTephridiae2Trial = dynamic(() => import('components/insects/dipteraTeph2'), {
-    ssr: false,});
-  
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  appBar: {
+    [theme.breakpoints.up("md")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: 0,
+    },
+  },
+  appBarBackground: {
+    backgroundColor: "#074880",
+    height: "100%",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  // toolbar: theme.mixins.toolbar,
+  // drawerPaper: {
+  //   width: drawerWidth,
+  //   backgroundColor: "red",
+  // },
+  closeIcon: {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+      flexDirection: "column",
+    },
+  },
 
+  link: {
+    textDecoration: "none",
+    color: "yellow",
+    fontSize: "14px",
+    fontFamily: "Open Sans",
+    fontWeight: 400,
+    textTransform: "uppercase",
+    marginLeft: theme.spacing(15),
+    "&:hover": {
+      color: "red",
+      borderBottom: "1px solid white",
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+      marginBottom: theme.spacing(1),
+    },
+  },
+  activeLink: {
+    color: "red",
+    borderBottom: "1px solid white",
+  },
+  // cancelicon: {
+  //   // position: 'absolute',
+  //   marginLeft: 7,
+  //   right: 0,
+  // },
+}));
 
-  // const DipteraTephridiaeredux= dynamic(() => import('components/insects/dipteraTephritidaeredux'), {
-  //   ssr: false,});
-    
-// const OtherDiptera = dynamic(() => import('components/insects/otherDiptera'), {
-//   ssr: false,
-// });
+export default function order(props) {
+  const classes = useStyles();
+  const handleMenuOpen = () => {
+    setMenuOpen(true);
+  };
 
-const Hymenoptera = dynamic(() => import('components/insects/hymenoptera'), {
-  ssr: false,
-});
-
-// const OtherHymenoptera = dynamic(() => import('components/insects/otherHymenoptera'), {
-//   ssr: false,
-// });
-
-export default function order(props){
-    return(
-    <Container>
-      <Box sx={{display:'flex',marginTop:'1rem',marginBottom:'1rem'}}>
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
+  const [menuOpen, setMenuOpen] = useState(false);
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  console.log("menuOpen");
+  return (
+    <Container sx={{ marginTop: 3 }}>
+      <Box sx={{ display: "flex" }}>
         <Box>
-        <DipteraTephridiae2Trial 
-        diptera_tephritideae_data={props.diptera_tephritideae_data}
-        other_diptera_data={props.other_diptera_data}/>
-       </Box>
-      
-       {/* <Box sx={{marginLeft:''}} >
-        <Lepidoptera lepidoptera_data={props.lepidoptera_data}/>
+          <DipteraTephridiae2Clientside />
+        </Box>
+        <LepidopteraClientside />
+        <Box>
+          <ColeopteraComponent />
         </Box>
 
-       <Box>
-        <Coleoptera coleoptera_data={props.coleoptera_data}/>
+        <Box>
+          <HymenopteraBraconidaeComponent />
+          <HymenopterahymenopteraIchneumonoidae />
+        </Box>
+        <OtherHymenopteracomponent />
       </Box>
-      <Box sx={{marginLeft:'1rem'}} >
-        <Hymenoptera 
-        hymenoptera_bracon_opiinae_data={props.hymenoptera_bracon_opiinae_data}
-        hym_ichumonoidae_data={props.hym_ichumonoidae_data}
-        other_hymenoptera_data={props.other_hymenoptera_data}
-        />
-      </Box> */}
-
-        </Box>
     </Container>
-    )
+  );
 }
-
-
-
-//fetch data for preparing page fior insects
-export async function getStaticProps(context){
-    console.log('context.params');        
-      // fetch coleoptera data
-      const dip_teph = await fetch('http://localhost:3000/api/insects/dipteraTephritidae/')
-      const diptera_tephritideae_data= await dip_teph.json()
-      console.log('diptera_tephritideae_data serverside')
-      console.log(diptera_tephritideae_data)
-  
-      // fetch lepidoptera_data data
-      const lep = await fetch('http://localhost:3000/api/insects/lepidoptera/')
-      const lepidoptera_data= await lep.json()
-      console.log('lepidoptera_data serverside')
-      console.log(lepidoptera_data)
-  
-     // fetch coleoptera data
-      const col = await fetch('http://localhost:3000/api/insects/coleoptera/')
-      const coleoptera_data= await col.json()
-      console.log('coleoptera_data serverside')
-      console.log(coleoptera_data)
-  
-    //   // fetch other diptera data
-      const other_dip = await fetch('http://localhost:3000/api/insects/otherDiptera/')
-      const other_diptera_data= await other_dip.json()
-      console.log('other_diptera_data serverside')
-      console.log(other_diptera_data)
-  
-            // fetch hymenoptera braconidae data
-      const hym_opin = await fetch('http://localhost:3000/api/insects/hymenopteraBraconidae/')
-      const hymenoptera_bracon_opiinae_data= await hym_opin.json()
-      console.log('hymenoptera_bracon_opiinae_data serverside')
-      console.log(hymenoptera_bracon_opiinae_data)
-  
-      // fetch hymenoptera data
-      const other_hym = await fetch('http://localhost:3000/api/insects/otherHymenoptera/')
-      const other_hymenoptera_data= await other_hym.json()
-      console.log('other_hymenoptera_data serverside')
-      console.log(other_hymenoptera_data)
-      //hymenopteraIchneumonoidae          
-          // fetch hymenoptera data
-      const hym_ichumonoidae = await fetch('http://localhost:3000/api/insects/hymenopteraIchneumonoidae/')
-      const hym_ichumonoidae_data= await hym_ichumonoidae.json()
-      console.log('other_hymenoptera_data serverside')
-      console.log(hym_ichumonoidae_data) 
-      return{
-      props:{
-       
-        lepidoptera_data,
-        diptera_tephritideae_data,
-        coleoptera_data,
-        other_diptera_data,
-        hym_ichumonoidae_data,
-        hymenoptera_bracon_opiinae_data,
-        other_hymenoptera_data
-      }
-    }
-  }
