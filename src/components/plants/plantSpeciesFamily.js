2e907dc0fd69608d6fbefca4187c7c9209a1eefb
bonfaceonyango <bonfaceb21@gmail.com>
2023-05-05 10:20:37 +0300

    updated plant family species

--- src/components/plants/plantSpeciesFamily.js
+++ src/components/plants/plantSpeciesFamily.js
@@ -1,33 +1,82 @@
 import { Box, Container, Stack } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+// const Index = () => {
+//   const router = useRouter()
+//   const {id} = router.query
+
+//   return(<div>{id}</div>)
+// }
 
 const useStyles = makeStyles((theme) => ({
   maroonText: {
-    color: 'maroon',
+    color: "maroon",
   },
   boldText: {
-    fontWeight: 'bold',
-    color: 'black',
+    fontWeight: "bold",
+    color: "black",
   },
 }));
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
-export default function PlantSpecieFamilyGenus({plants_data}) {
+export default function PlantSpecieFamilyGenus(props) {
+  // const [familyData, setFamilyData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  // const [error, setError] = useState(null);
+  const router = useRouter();
+  // const species = router.query.speciesName;
+  // Set the default species to "abutilum hirtum"
+  const species = parseInt(router.query.species);
   const classes = useStyles();
+  console.log("species plantfamily component");
+  console.log(typeof species);
+  // useEffect(() => {
+  //   fetch(`/api/plantsPage/${species}`)
+  //     .then((res) => res.json())
+  //     .then(
+  //       (result) => {
+  //         setLoaded(true);
+  //         setFamilyData(result);
+  //       },
+  //       (error) => {
+  //         setLoaded(true);
+  //         setError(error);
+  //       }
+  //     );
+  // }, []);
+  // const { data, error } = useSWR(key, fetcher);
+  // const base_url = "http://localhost:3000";
 
-  return (
-    plants_data.map((specie,index)=>
-    <Box key={index} sx={{ marginTop: "0rem" }}>
-    <span className={classes.maroonText}>
-      {specie.plant_genera.genus_name + " "}{specie.species_name}
-    </span>
-    <br/>
-    <span className={classes.boldText}>
-      Family:
-    </span>
-    <span className={classes.maroonText}>
-      {" " + specie.plant_genera.plant_families.family_name}
-    </span>
-  </Box>
-    )
+  const base_url="http://192.168.43.93:3000"
+
+  const { data, error } = useSWR(
+    `${base_url}/api/plantsPage/${species}`,
+    fetcher
   );
+  // fetch(`/api/plantsPage/${species}`)
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+  console.log("familyData");
+  console.log(data);
+  console.log("species");
+  console.log(species);
+  // return data;
+  return data.map((specie, index) => (
+    <Box key={index} sx={{ marginTop: "0rem", fontWeight: "bold" }}>
+      Species:
+      <span className={classes.maroonText}>
+        {specie.plant_genera.genus_name + " "}
+        {specie.species_name}
+      </span>
+      <br />
+      <span className={classes.boldText}>Family:</span>
+      <span className={classes.maroonText}>
+        {" " + specie.plant_genera.plant_families.family_name}
+      </span>
+    </Box>
+  ));
 }
