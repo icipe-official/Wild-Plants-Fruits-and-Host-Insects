f167bd92de8f97006e082d7fcf828bfb33b9775e
bonfaceonyango <bonfaceb21@gmail.com>
2023-05-20 16:51:25 +0300

    updated photo component

---
+++ src/components/plants/photos/photosmain.js
@@ -0,0 +1,137 @@
+import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+// import Image from "next/legacy/image";
+import Image from "next/image";
+
+import styles from "./photos.module.css";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+//This component will be receiving data from getstaticprops
+export default function PhotosComponent({ photos_data, selectedIndex }) {
+  const [photoData, setphotoData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  //control next and previous image
+  const [currentImage, setCurrentImage] = useState(0);
+
+  // const [error, setError] = useState(null);
+
+  // console.log("photos");
+  // console.log(photos);
+  const [showImage, setShowImage] = useState({
+    showImageNow: true,
+    showImageId: null,
+  });
+  const [open, setOpen] = useState(0);
+
+  //manfe view of image on exapnde state
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpandImage = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  //   const router = useRouter();
+  //   const species = router.query.speciesName;
+  //   console.log("species");
+  //   console.log(species);
+  // fetch(`/api/plantsPage/${species}`)
+  // if (error) return <div>Failed to load</div>;
+  // if (!data) return <div>Loading...</div>;
+  console.log(" photo");
+  console.log(photos_data);
+  if (photos_data) {
+    const photos = photos_data
+      .map((plant) => plant.plants_photos)
+      [selectedIndex].map((p) => p.photo_name);
+    console.log(photos);
+    //check if length of photos is more than one
+    if (photos.length === 1) {
+      return (
+        <Box sx={{ marginTop: "1rem", marginLeft: 0 }}>
+          {photos.map((specie) => (
+            <Box key={specie.id}>
+              <Image
+                src={`/plants/${specie}`}
+                alt="Picture of the plant"
+                width={500}
+                height={400}
+                layout="responsive"
+              />
+            </Box>
+          ))}
+        </Box>
+      );
+    } else if (photos.length === 0) {
+      return (
+        <Box sx={{ marginTop: "1rem", paddingBottom: 2 }}>
+          <Image
+            src="/plants/noImage.jpg"
+            alt="No Image"
+            width={500}
+            height={400}
+            layout="responsive"
+          />
+        </Box>
+      );
+    } else {
+      const previousImage = () => {
+        setCurrentImage(
+          currentImage === 0 ? photos.length - 1 : currentImage - 1
+        );
+      };
+
+      const nextImage = () => {
+        setCurrentImage(
+          currentImage === photos.length - 1 ? 0 : currentImage + 1
+        );
+      };
+      const OpenImage = (open) => {
+        if (open === photos.length - 1) {
+          setOpen(0);
+        } else {
+          setOpen(open + 1);
+        }
+      };
+      return (
+        // <Box sx={{ marginTop: "1rem", paddingBottom: 2 }}>
+        //   <Image
+        //     src={`/plants/${photos[open]}`}
+        //     alt="No Image"
+        //     width={500}
+        //     height={400}
+        //     layout="responsive"
+        //   />
+        //   <span>
+        //     {open + 1}/{photos.length}
+        //   </span>
+        //   <Box>
+        //     <button onClick={() => OpenImage(open)} className="ground">
+        //       Next Image
+        //     </button>
+        //   </Box>
+        // </Box>
+
+        <Box sx={{ marginTop: "1rem", paddingBottom: 2 }}>
+          <Image
+            src={`/plants/${photos[currentImage]}`}
+            alt="No Image"
+            width={500}
+            height={400}
+            layout="responsive"
+          />
+          <span>
+            {currentImage + 1}/{photos.length}
+          </span>
+          <Box>
+            <button onClick={previousImage} className="ground">
+              &lt;
+            </button>
+            <button onClick={nextImage} className="ground">
+              &gt;
+            </button>
+          </Box>
+        </Box>
+      );
+    }
+  }
+}
