818eae83cafedb48df68bb948dc47112df7f3f43
bonfaceonyango <bonfaceb21@gmail.com>
2023-05-20 18:35:01 +0300

    glossary api

---
+++ src/pages/api/glossary/glossary.js
@@ -0,0 +1,30 @@
+import prisma from "../../../../lib/prisma";
+import NextCors from "nextjs-cors";
+export default async function handle(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  if (req.method == "GET") {
+    try {
+      // get family data.
+      const glossary = await prisma.glossary.findMany({
+        select: {
+          glossary_id: true,
+          glossary_type: true,
+          glossary_term: true,
+          glossary_description: true,
+
+          glossary_photos: {
+            select: { photo_id: true },
+          },
+        },
+      });
+      return res.json(glossary);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
