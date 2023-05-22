b9cdbcd3dc74a8c3db2dd9357acfae3bc4e615a8
bonfaceonyango <bonfaceb21@gmail.com>
2023-05-22 14:56:05 +0300

    photos component

--- src/components/plants/photos/photosmain.js
+++ src/components/plants/photos/photosmain.js
@@ -3,9 +3,6 @@ import { useState, useEffect } from "react";
 // import Image from "next/legacy/image";
 import Image from "next/image";
 
-import styles from "./photos.module.css";
-import { useRouter } from "next/router";
-import useSWR from "swr";
 //This component will be receiving data from getstaticprops
 export default function PhotosComponent({ photos_data, selectedIndex }) {
   const [photoData, setphotoData] = useState([]);
@@ -31,81 +28,90 @@ export default function PhotosComponent({ photos_data, selectedIndex }) {
   };
   console.log(" photo");
   console.log(photos_data);
-  if (photos_data) {
-    const photos = photos_data
-      .map((plant) => plant.plants_photos)
-      [selectedIndex].map((p) => p.photo_name);
-    console.log(photos);
-    //check if length of photos is more than one
-    if (photos.length === 1) {
-      return (
-        <Box sx={{ marginTop: "1rem", marginLeft: 0 }}>
-          {photos.map((specie) => (
-            <Box key={specie.id}>
-              <Image
-                src={`/plants/${specie}`}
-                alt="Picture of the plant"
-                width={500}
-                height={400}
-                layout="responsive"
-              />
-            </Box>
-          ))}
-        </Box>
-      );
-    } else if (photos.length === 0) {
-      return (
-        <Box sx={{ marginTop: "1rem", paddingBottom: 2 }}>
-          <Image
-            src="/plants/noImage.jpg"
-            alt="No Image"
-            width={500}
-            height={400}
-            layout="responsive"
-          />
-        </Box>
-      );
-    } else {
-      const previousImage = () => {
-        setCurrentImage(
-          currentImage === 0 ? photos.length - 1 : currentImage - 1
-        );
-      };
-
-      const nextImage = () => {
-        setCurrentImage(
-          currentImage === photos.length - 1 ? 0 : currentImage + 1
-        );
-      };
-      const OpenImage = (open) => {
-        if (open === photos.length - 1) {
-          setOpen(0);
-        } else {
-          setOpen(open + 1);
-        }
-      };
-      return (
-        <Box sx={{ marginTop: "1rem", paddingBottom: 2 }}>
-          <Image
-            src={`/plants/${photos[currentImage]}`}
-            alt="No Image"
-            width={500}
-            height={400}
-            layout="responsive"
-          />
-          <span>
-            {currentImage + 1}/{photos.length}
-          </span>
-          <Box>
-            <button onClick={previousImage} className="ground">
-              &lt;
-            </button>
-            <button onClick={nextImage} className="ground">
-              &gt;
-            </button>
+  try {
+    if (photos_data) {
+      const photos = photos_data
+        .map((plant) => plant.plants_photos)
+        [selectedIndex].map((p) => p.photo_name);
+      console.log(photos);
+      //check if length of photos is more than one
+      if (photos.length === 1) {
+        return (
+          <Box sx={{ marginTop: "1rem", marginLeft: 0 }}>
+            {photos.map((specie) => (
+              <Box key={specie.id}>
+                <Image
+                  src={`/plants/${specie}`}
+                  alt="Picture of the plant"
+                  width={500}
+                  height={400}
+                  layout="responsive"
+                />
+              </Box>
+            ))}
           </Box>
-        </Box>
-      );
+        );
+      } else if (photos.length === 0) {
+        return (
+          <Box sx={{ marginTop: "1rem", paddingBottom: 2 }}>
+            <Image
+              src="/plants/noImage.jpg"
+              alt="No Image"
+              width={500}
+              height={400}
+              layout="responsive"
+            />
+          </Box>
+        );
+      } else {
+        const previousImage = () => {
+          setCurrentImage(
+            currentImage === 0 ? photos.length - 1 : currentImage - 1
+          );
+        };
+
+        const nextImage = () => {
+          setCurrentImage(
+            currentImage === photos.length - 1 ? 0 : currentImage + 1
+          );
+        };
+        const OpenImage = (open) => {
+          if (open === photos.length - 1) {
+            setOpen(0);
+          } else {
+            setOpen(open + 1);
+          }
+        };
+        return (
+          <Box sx={{ marginTop: "1rem", paddingBottom: 2 }}>
+            <Image
+              src={`/plants/${photos[currentImage]}`}
+              alt="No Image"
+              width={500}
+              height={400}
+              layout="responsive"
+            />
+            <span>
+              {currentImage + 1}/{photos.length}
+            </span>
+            <Box>
+              <button onClick={previousImage} className="ground">
+                &lt;
+              </button>
+              <button onClick={nextImage} className="ground">
+                &gt;
+              </button>
+            </Box>
+          </Box>
+        );
+      }
     }
+  } catch (error) {
+    console.error("Error:", error);
+    return (
+      <Box sx={{ marginTop: "1rem", paddingBottom: 2 }}>
+        <span>No photos found</span>
+      </Box>
+    );
   }
 }
