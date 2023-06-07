4327fb0ee62b5e25da87b71aef430eed2540e2f9
bonfaceonyango <bonfaceb21@gmail.com>
2023-06-07 13:04:09 +0300

    updated plants api

--- src/pages/api/plants/species.js
+++ src/pages/api/plants/species.js
@@ -5,27 +5,23 @@
 // It also joins family table and retrieve family name of all plants.
 // The query results are then returned as a JSON response to the client.
 // Try catch block is used to catch the error during get request
-
-import prisma from "../../../../lib/prisma";
+import prisma from "../../../lib/prisma";
 import NextCors from "nextjs-cors";
+
 export default async function handle(req, res) {
-  // Run the cors middleware
-  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
   await NextCors(req, res, {
-    // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: "*",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200,
   });
-  if (req.method == "GET") {
-    // get species data.
+
+  if (req.method === "GET") {
     try {
       const species = await prisma.plants.findMany({
         select: {
           id: true,
           species_name: true,
           genus_id: true,
-          matk: true,
           plant_genera: {
             select: {
               genus_name: true,
@@ -40,9 +36,17 @@ export default async function handle(req, res) {
           },
           plants_matk: {
             select: {
+              matk_id: true,
               nucleotide: true,
               country: true,
               genebank_accession: true,
+            },
+          },
+        },
+        orderBy: {
+          plant_genera: {
+            plant_families: {
+              family_name: "asc",
             },
           },
         },
