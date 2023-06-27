341858c36366b3405f111d4424997a8fb6608d1b
bonfaceonyango <bonfaceb21@gmail.com>
2023-06-27 17:41:43 +0300

    updated plant componrnt

--- src/pages/plants/[species].js
+++ src/pages/plants/[species].js
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import FruitDetailsFeaturesValues from 'components/plants/plantFruitLeafValues';
-import { Container } from '@mui/material';
+import FruitDetailsFeaturesValues from "components/plants/plantFruitLeafValues";
+import { Container } from "@mui/material";
 
 export default function PlantPage() {
   // const router = useRouter();
