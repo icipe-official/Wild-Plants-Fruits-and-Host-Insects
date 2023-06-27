ce8dfedcd6c63f4361976a19c9a0352ae9dece31
bonfaceonyango <bonfaceb21@gmail.com>
2023-06-27 17:26:50 +0300

    updated plant componrnt

--- src/components/plants/plantSpeciesFamilyResult.js
+++ src/components/plants/plantSpeciesFamilyResult.js
@@ -1,6 +1,5 @@
-import { Box, Container, Stack } from "@mui/material";
+import { Box, Container, Stack, useMediaQuery } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import { useMediaQuery } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   maroonText: {
@@ -13,27 +12,27 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function PlantSpecieFamilyGenusResult({ plants_data }) {
-  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const isSmallScreen = useMediaQuery(`(max-width: 1282px)`);
 
   const classes = useStyles();
   return plants_data.map((specie) => (
     <Box
       key={specie.id}
       sx={{
-        marginTop: 2,
+        marginTop: isSmallScreen ? 2 : 0,
         marginLeft: isSmallScreen ? "0" : "8rem",
         fontWeight: "bold",
       }}
     >
       Species:
       <span className={classes.maroonText}>
-        {specie.plant_genera.genus_name + " "}
+        {`${specie.plant_genera.genus_name} `}
         {specie.species_name}
       </span>
       <br />
       <span className={classes.boldText}>Family:</span>
       <span className={classes.maroonText}>
-        {" " + specie.plant_genera.plant_families.family_name}
+        {` ${specie.plant_genera.plant_families.family_name}`}
       </span>
     </Box>
   ));
