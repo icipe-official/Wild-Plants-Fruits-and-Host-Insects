698502323249227fbb096969c4a6c9be90790d5d
bonfaceonyango <bonfaceb21@gmail.com>
2023-07-28 11:07:37 +0300

    updated glossary api

--- src/pages/api/glossary/glossary.js
+++ src/pages/api/glossary/glossary.js
@@ -1,25 +1,29 @@
-import prisma from "../../../lib/prisma";
 import NextCors from "nextjs-cors";
+import prisma from "../../../lib/prisma";
+
 export default async function handle(req, res) {
   await NextCors(req, res, {
     // Options
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    methods: ["DELETE", "GET", "HEAD", "PATCH", "POST", "PUT"],
     origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
-  if (req.method == "GET") {
+
+  if (req.method === "GET") {
     try {
-      // get family data.
+      // Get family data.
       const glossary = await prisma.glossary.findMany({
         select: {
-          glossary_id: true,
-          glossary_type: true,
-          glossary_term: true,
           glossary_description: true,
-
+          glossary_id: true,
           glossary_photos: {
             select: { photo_id: true },
           },
+          glossary_term: true,
+          glossary_type: true,
+        },
+        orderBy: {
+          glossary_term: "asc",
         },
       });
       return res.json(glossary);
