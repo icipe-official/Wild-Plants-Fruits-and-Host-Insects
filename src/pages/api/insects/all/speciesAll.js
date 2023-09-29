94be1d128bb79726ba9b42929d83efabaa356bd2
bonfaceonyango <bonfaceb21@gmail.com>
2023-09-29 14:46:02 +0300

    updated genbank

--- src/pages/api/insects/all/speciesAll.js
+++ src/pages/api/insects/all/speciesAll.js
@@ -47,7 +47,7 @@ export default async function getColeoptera(req, res) {
           select: {
             nucleotide: true,
             country: true,
-            genebank_accesion: true,
+            genbank_accession: true,
           },
         },
         plants_insects: {
