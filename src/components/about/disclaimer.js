393a6665ff8394738f9cc73452306a777f056f64
bonfaceonyango <bonfaceb21@gmail.com>
2023-10-11 19:18:11 +0300

    updated about us

---
+++ src/components/about/disclaimer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Description } from "@mui/icons-material";
+import { Box, Container, Divider } from "@mui/material";
+
+export default function Disclaimer() {
+  return (
+    <Container>
+      <Box sx={{ marginTop: 12, justifyContent: "center" }}>
+        <h3>
+          <a name="disclaimer" class="anchor">
+            Disclaimer
+          </a>
+        </h3>
+        The barcodes that have been used for constructing the phylogeny tree of
+        respective biocollections were obtained from the BOLD database with
+        assumption that taxonomically identified individuals share identical
+        barcodes with the species that have been barcoded. The link to the
+        barcodes are provided upon clicking each species in the phylogeny tree
+      </Box>
+      {/* <Divider /> */}
+    </Container>
+  );
+}
