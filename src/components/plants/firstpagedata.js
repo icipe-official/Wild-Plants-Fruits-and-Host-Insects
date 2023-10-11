f547e051be3e1ed9f126e64d93a199b9169b9a13
bonfaceonyango <bonfaceb21@gmail.com>
2023-10-11 20:31:07 +0300

    updated Nav bar to add about us

---
+++ src/components/plants/firstpagedata.js
@@ -0,0 +1,185 @@
+const data = {
+  id: 1,
+  species_name: "hirtum (Lam.) Sweet",
+  max_latitude: 1350,
+  min_latitude: 1,
+  plant_description: "nan",
+  leaf_type: "simple",
+  plant_genera: {
+    genus_name: "Abutilon",
+  },
+  plant_families: {
+    family_name: "Malvaceae",
+  },
+  plants_woody_herbaceous: [
+    {
+      back_id: 3,
+      woody_herbaceous: {
+        type_of_stem: "woody",
+      },
+    },
+  ],
+  plants_latex: [
+    {
+      fruit_latex_id: 1,
+      latex: {
+        latex_description: "present",
+      },
+    },
+  ],
+  plants_spines_thorns: [
+    {
+      thorn_id: 3,
+      spines_thorns: {
+        thorns_spines: "absent",
+      },
+    },
+  ],
+  plants_shrub_climbings: [
+    {
+      shrub_id: 5,
+      shrub_climbings: {
+        tree_type: "herb",
+      },
+    },
+    {
+      shrub_id: 8,
+      shrub_climbings: {
+        tree_type: "shrub",
+      },
+    },
+  ],
+  plants_leaf_margins: [
+    {
+      leaf_margin_id: 4,
+      leaf_margins: {
+        type_of_leaf_margin: "serrate",
+      },
+    },
+    {
+      leaf_margin_id: 9,
+      leaf_margins: {
+        type_of_leaf_margin: "crenate",
+      },
+    },
+  ],
+  plants_leaf_arrangements: [
+    {
+      leaf_arrangement_id: 3,
+      leaf_arrangements: {
+        arrangement: "alternate",
+      },
+    },
+  ],
+  plants_fruit_types: [
+    {
+      fruit_type_id: 2,
+      fruit_types: {
+        type_of_fruit: "multiple",
+      },
+    },
+  ],
+  plants_fruit_colors: [
+    {
+      color_id: 5,
+      fruit_colors: {
+        color: "green",
+      },
+    },
+  ],
+  plants_fruit_shapes: [
+    {
+      fruit_shape_id: 6,
+      fruit_shapes: {
+        shape: "cylindrical",
+      },
+    },
+  ],
+  plant_coordinates: [
+    {
+      latitude: "0.83835",
+      longitude: "37.44605",
+    },
+  ],
+  plants_fruit_sizes: [
+    {
+      fruit_size_id: 3,
+      fruit_sizes: {
+        size: "15-25 mm",
+      },
+    },
+  ],
+  plants_fruiting_months: [
+    {
+      fruiting_month_id: 1,
+      fruiting_months: {
+        month: "FEB",
+      },
+    },
+  ],
+  plants_ukwf_areas: [
+    {
+      ukwf_area_id: 5,
+      ukwf_areas: {
+        ukwf_area: "unknown",
+      },
+    },
+  ],
+  plants_k_sectors: [
+    {
+      k_sector_id: 1,
+      k_sectors: {
+        sector: "1",
+      },
+    },
+    {
+      k_sector_id: 2,
+      k_sectors: {
+        sector: "2",
+      },
+    },
+    {
+      k_sector_id: 3,
+      k_sectors: {
+        sector: "3",
+      },
+    },
+    {
+      k_sector_id: 4,
+      k_sectors: {
+        sector: "4",
+      },
+    },
+    {
+      k_sector_id: 6,
+      k_sectors: {
+        sector: "6",
+      },
+    },
+    {
+      k_sector_id: 7,
+      k_sectors: {
+        sector: "7",
+      },
+    },
+  ],
+  plants_photos: [
+    {
+      photo_name: "01780.jpg",
+    },
+    {
+      photo_name: "01781.jpg",
+    },
+    {
+      photo_name: "01782.jpg",
+    },
+  ],
+  plants_regions: [
+    {
+      regions: {
+        latitude: "0.4612",
+        longitude: "39.6401",
+      },
+    },
+  ],
+};
