31ecc698e6547285c73cc62f1d388c25b2d728ef
bonfaceonyango <bonfaceb21@gmail.com>
2023-12-07 15:08:43 +0300

    updated glossary page

--- src/components/glossary/glossaryTerms.js
+++ src/components/glossary/glossaryTerms.js
@@ -19,6 +19,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import PhotosComponent from "components/plants/photos/photosmain";
+import { transcode } from "buffer";
 const useStyles = makeStyles({
   root: {
     display: "flex",
@@ -59,21 +60,7 @@ const useStyles = makeStyles({
 export default function GlossaryAll() {
   const classes = useStyles();
   const fetcher = (url) => fetch(url).then((r) => r.json());
-  //set query parameters
-  const [query, setQuery] = useState({
-    shrub_climbings: [],
-    plant_spines: [],
-    plant_latex: [],
-    fruit_types: [],
-    fruit_colors: [],
-    fruit_sizes: [],
-    fruit_shapes: [],
-    leaf_types: [],
-    leaf_margins: [],
-    leaf_arrangements: [],
-  });
-  // state of selcted plant and glossary details
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  //image list
 
   // const baseUrl = "http://localhost:3000"; // set the base URL here
 
@@ -94,142 +81,67 @@ export default function GlossaryAll() {
     isLoading: fruit_isloading,
   } = useSWR(`${base_url}/api/glossary/glossary`, fetcher);
 
-  const { data: example, error: specieserror1 } = useSWR(
-    // pass the json stringified query
-    `${base_url}/api/multipleEntry/multipleentry?filters=${JSON.stringify(
-      query
-    )}`,
-    fetcher,
-    {
-      revalidateOnMount: true, // Fetch data on component mount
-      // revalidateOnMount: false, // Fetch data on component mount
-    }
-  );
-
   const router = useRouter();
   const [showPhotos, setShowPhotos] = useState(false);
   const [selectedType, setSelectedType] = useState(null);
   const [selectedTerm, setSelectedTerm] = useState(false);
+  const [uniqueIndex, setuniqueIndex] = useState(0);
+  // state of selcted plant and glossary details
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   // const [query, setQuery] = useState({ fruit_types: "" });
   const [filterdspecies, setFiltrerdsepcies] = useState(null);
-  const glossaryTerm = router.query.glossary_term;
+  const glossaryTerm = router.query;
 
-  //handle cancel
-  function cancel() {
-    //states for query
-    // setQuery((prevQueryInfo) => ({ ...prevQueryInfo, plant_types: [] })); //setting state in dictionary
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, plant_latex: [] })); //setting state in dictionary
-    // setQuery(prevQueryInfo => ({...prevQueryInfo,plant_thorns:[]})) //setting state in dictionary
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, fruit_colors: [] })); //setting state in dictionary
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, fruit_shapes: [] })); //setting state in dictionary
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, fruit_sizes: [] })); //setting state in dictionary
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, fruit_types: [] })); //setting state in dictionary
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, leaf_types: [] })); //setting state in dictionary
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, leaf_margins: [] })); //setting state in dictionary
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, leaf_arrangements: [] }));
-    setQuery((prevQueryInfo) => ({ ...prevQueryInfo, shrub_climbings: [] }));
-  }
   const handleItemClick = (specie, index) => {
     setSelectedIndex(index);
+
+    setuniqueIndex(index);
   };
 
   const handleDoubleClick = (specie, index) => {
     // const url = `/plants/${specie.id}`;
     // setSelectedIndex(index);
-    router.push(
-      `/plants/${specie.id}?familyName=${specie.plant_genera.plant_families?.family_name}&genusName=${specie}`
-    );
+    router.push(`/plants/${specie.plants_photos.plant_id}`);
   };
 
   //handle on click event of the image
-  function handleClick(type) {
-    // if (typeof currentQuery !== "undefined") {
-    //   setShowPhotos(true);
-    // }
-    // console.log(type);
-    cancel();
+  function handleClick(type, index) {
+    setSelectedTerm(true);
     if (type?.glossary_term) {
-      ////console.log("type on click");
-      ////console.log(type);
-      ////console.log(showPhotos);
-      ////console.log("example");
-
-      ////console.log(example);
-      // console.log("type");
-
-      // console.log(type);
+      // Update the selected index
+      // setSelectedIndex(clickedSpeciesIndex);
       setSelectedType(type);
       setShowPhotos(true);
       setSelectedTerm(true);
-      setQuery((prevQueryInfo) => ({
-        ...prevQueryInfo,
-        [type.glossary_type + "s"]: type?.glossary_term.toLowerCase(),
-      }));
-      // console.log("selectedType");
-
-      // console.log(selectedType);
-      //empty the query
+      setSelectedIndex(index ? index : 0);
+      setuniqueIndex(index ? index : 0);
     }
+    // console.log("type");
+    // console.log(type);
+    // setuniqueIndex(index ? index : 0);
   }
-  useEffect(() => {
-    cancel();
 
-    if (glossary && glossaryTerm) {
-      const filteredType = router.query;
-      //   (type) =>
-      //     type.glossary_term.toLowerCase() === glossaryTerm.toLowerCase()
-      // );
-      if (filteredType) {
-        handleClick(filteredType);
+  useEffect(() => {
+    if (glossaryTerm) {
+      // setShowPhotos(true);
+      console.log("glossaryTerm5");
+      // console.log("glossaryTerm5");
+
+      const value = Object.values(glossaryTerm)[0];
+      // console.log(value.charAt(0).toUpperCase() + value.slice(1));
+      const filteredItems = glossary?.filter((item) =>
+        item.glossary_term.includes(
+          value.charAt(0).toUpperCase() + value.slice(1)
+        )
+      );
+      console.log("glossary");
+      console.log(filteredItems);
+      if (typeof filteredItems !== null) {
+        handleClick(filteredItems);
       }
     }
   }, [glossary, glossaryTerm]);
-
-  useEffect(() => {
-    const currentQuery = { ...router.query };
-
-    // Iterate over the query parameters
-    // if (typeof currentQuery !== "undefined") {
-    for (const key in currentQuery) {
-      const value = currentQuery[key];
-
-      // Update the corresponding key in the query state dynamically
-      if (key in query) {
-        setQuery((prevQuery) => ({
-          ...prevQuery,
-          [key]: Array.isArray(value) ? value : [value],
-        }));
-      }
-
-      // Extract the dynamic URL parameter
-      const dynamicParam = Object.values(query).find(
-        (value) => typeof value === "string"
-      );
-      console.log("dynamicParam");
-      //pass in key e.g fruit_types and value e.g achene to the handleclick function
-      // console.log(key);
-
-      let typeCapitalize = value.charAt(0).toUpperCase() + value.slice(1);
-
-      // Filter the glossary items based on the conditions
-
-      const filterdGlossary = glossary?.filter((item) => {
-        return (
-          item.glossary_type === key.replace(/s$/, "") &&
-          item.glossary_term === typeCapitalize
-        );
-      });
-
-      // setSelectedType(filterdGlossary);
-      if (typeof filterdGlossary !== "undefined") {
-        // console.log(filterdGlossary);
-        // Call handleClick with the dynamicParam
-        handleClick(filterdGlossary[0]);
-      }
-    }
-
-    // setShowPhotos(true);
-  }, [router.query]);
 
   const isSmallScreen = useMediaQuery(`(max-width: 1282px)`);
   // console.log(query);
@@ -237,6 +149,7 @@ export default function GlossaryAll() {
     setSelectedType(null);
     setShowPhotos(false);
   }
+
   if (fruit_typerror)
     return (
       <Container
@@ -275,160 +188,195 @@ export default function GlossaryAll() {
       </Container>
     );
 
-  if (glossary && example) {
-    const filtered_examples = example.filter((specie) => {
-      ////console.log(" of the example");
+  if (glossary) {
+    console.log("glossaryTerm22");
+    console.log("glossaryTerm22");
+    console.log(glossaryTerm);
 
-      // ////console.log(specie);
-      if (specie.plants_photos.length > 0) {
-        return specie;
-      }
-    });
-    ////console.log("example");
+    // console.log(glossary.glossary_examples);
 
-    ////console.log(example.map((sp) => sp.plants_photos).length > 0);
+    if (selectedTerm !== null) {
+      // Create a set to store unique plant IDs
+      const uniquePlantIds = new Set();
 
-    ////console.log("filtered_examples");
+      // Create an array to store unique species
+      var uniqueSpecies = [];
 
-    ////console.log(filtered_examples);
-    ////console.log("photos");
-    ////console.log(filtered_examples.map((photo) => photo.plants_photos));
-    // \\use the selected index status to get the specific photls
-    // const quey=
-    const photos = filtered_examples.map((photo) => photo.plants_photos)[
-      selectedIndex
-    ];
-    console.log("individual photos");
+      // Iterate through each glossary term in the data
+      selectedType?.glossary_examples.forEach((glossaryTerm) => {
+        console.log("glossaryTerm");
 
-    // console.log(query);
-    //handle clicking of example species
+        console.log(glossaryTerm);
+        // Check if 'glossaryTerm.glossary_examples' is an array
 
-    return (
-      <Container>
-        {/* filter only terms with description*/}
-        <Box sx={{ fontWeight: "bold", marginTop: 9 }}>Alphabetical</Box>
-        {glossary
-          .filter((t) => Boolean(t.glossary_description))
-          .map((type) => (
-            <Grid container alignItems="center">
-              <Grid item xs={true}>
-                <div className={classes.dynamicValueCard}>
-                  <DynamicValueCard
-                    key={type.id} // Add a unique key prop
-                    value={
-                      type.glossary_term.replace(/_/g, " ") +
-                      " (" +
-                      type.glossary_type.replace(/_/g, " ") +
-                      "): " +
-                      type.glossary_description
-                    }
-                  />
+        // Iterate through each example in the array
+        selectedType.glossary_examples.forEach((example) => {
+          console.log("example");
 
-                  {/* </Box> */}
-                </div>
+          console.log(example);
+          // Check if the plant ID is not in the set, then add it
+          if (!uniquePlantIds.has(example.plants_photos.plant_id)) {
+            uniquePlantIds.add(example.plants_photos.plant_id);
+
+            // Add the unique species to the array
+            uniqueSpecies.push(example.plants_photos);
+            console.log("uniqueSpecies", uniqueSpecies);
+          }
+        });
+      });
+      const photos_data = selectedType?.glossary_examples?.filter(
+        (plant) =>
+          plant?.plants_photos?.plant_id ===
+          uniqueSpecies[selectedIndex]?.plant_id
+      );
+      console.log("photos_data");
+
+      console.log(uniqueIndex);
+
+      const transformedData = [
+        {
+          // setShowPhotos(true);
+
+          plants_photos: photos_data?.map((item) => ({
+            photo_name: item.photo_name,
+            plants: Object.values(item.plants_photos).map((plant) => ({
+              plant_id: plant.plant_id,
+            })),
+          })),
+        },
+      ];
+
+      console.log("transformedData");
+
+      console.log(selectedIndex);
+
+      // console.log("uniqueSpecies");
+
+      // console.log(uniqueSpecies);
+      // console.log("selectedType");
+
+      // console.log(selectedType);
+
+      return (
+        <Container>
+          {/* filter only terms with description*/}
+          <Box sx={{ fontWeight: "bold", marginTop: 9 }}>Alphabetical</Box>
+          {glossary
+            .filter((t) => Boolean(t.glossary_description))
+            .map((type, index) => (
+              <Grid container alignItems="center">
+                <Grid item xs={true}>
+                  <div className={classes.dynamicValueCard}>
+                    <DynamicValueCard
+                      key={type.id} // Add a unique key prop
+                      value={
+                        type.glossary_term.replace(/_/g, " ") +
+                        " (" +
+                        type.glossary_type.replace(/_/g, " ") +
+                        "): " +
+                        type.glossary_description
+                      }
+                    />
+                    {/* </Box> */}
+                  </div>
+                </Grid>
+                <Grid item>
+                  <Button
+                    sx={{ textDecoration: "none" }}
+                    onClick={() => handleClick(type)}
+                    color="success"
+                  >
+                    Images
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Button
-                  sx={{ textDecoration: "none" }}
-                  onClick={() => handleClick(type)}
-                  color="success"
-                >
-                  Images
-                </Button>
-              </Grid>
-            </Grid>
-          ))}
-        {/* </Box> */}
+            ))}
+          {/* </Box> */}
 
-        {showPhotos && (
-          <Box
-            sx={{
-              position: "fixed",
-              top: "40%",
-              left: "40%",
-              transform: "translate(-40%, -50%)",
-              backgroundColor: "lightgrey",
-              padding: "1rem",
-              maxHeight: "80%",
-              maxWidth: "100%", // Adjusted maxWidth to occupy around 3/4 of the screen
-              overflowY: "auto",
-              marginTop: 10,
-            }}
-          >
-            {/* <IconButton onClick={handleClose} sx={{ position: "absolute", top: 0, right: 0 }}>
+          {showPhotos && (
+            <Box
+              sx={{
+                position: "fixed",
+                top: "40%",
+                left: "40%",
+                transform: "translate(-40%, -50%)",
+                backgroundColor: "lightgrey",
+                padding: "1rem",
+                maxHeight: "80%",
+                maxWidth: "100%", // Adjusted maxWidth to occupy around 3/4 of the screen
+                overflowY: "auto",
+                marginTop: 10,
+              }}
+            >
+              {/* <IconButton onClick={handleClose} sx={{ position: "absolute", top: 0, right: 0 }}>
         <CloseIcon/>
       </IconButton>
             <Button sx={{ color: "green" }} onClick={handleClose}>
             
               Close
             </Button> */}
-            <Button
-              type="button"
-              class="close"
-              aria-label="Close"
-              onClick={handleClose}
-            >
-              <span aria-hidden="true">&times;</span>
-            </Button>
-            <Box sx={{ marginLeft: 0 }}>
-              <Box sx={{ color: "red" }}> {selectedType?.glossary_term} </Box>
-              <Box> {selectedType?.glossary_description}</Box>
-              <Box>
+              <Button
+                type="button"
+                class="close"
+                aria-label="Close"
+                onClick={handleClose}
+              >
+                <span aria-hidden="true">&times;</span>
+              </Button>
+              <Box sx={{ marginLeft: 0 }}>
+                <Box sx={{ color: "red" }}> {selectedType?.glossary_term} </Box>
+                <Box> {selectedType?.glossary_description}</Box>
                 <Box>
                   <Box>
-                    {/* <Image
+                    <Box>
+                      {/* <Image
                       src={`/plants/${selectedType.photo_id}`}
                       alt="Picture of the plant"
                       width={300}
                       height={200}
                       layout="responsive"
                     /> */}
-                    <PhotosComponent
-                      photos_data={filtered_examples}
-                      selectedIndex={selectedIndex}
-                    />
+                      <PhotosComponent
+                        photos_data={transformedData}
+                        selectedIndex={0}
+                      />
+                    </Box>
                   </Box>
                 </Box>
-              </Box>
 
-              <Box sx={{ fontWeight: "bold" }}> Examples </Box>
-              <Box>
-                {" "}
-                {filtered_examples.map((specie, index) => (
-                  <Box
-                    sx={{
-                      backgroundColor:
-                        index === 0 && selectedIndex !== 0
-                          ? "inherit"
-                          : selectedIndex === index
-                          ? "gray"
-                          : "inherit",
-                      color: selectedIndex === index ? "white" : "black",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => handleItemClick(specie, index)}
-                    onDoubleClick={() => handleDoubleClick(specie, index)}
-                  >
-                    {/* <Link
-                      key={specie.id}
-                      href="/plants/[species]"
-                      as={`/plants/${specie.id}`}
-                      passHref
-                      // href={`/plants/${specie.species_name}`}
-                      style={{ textDecoration: "none", color: "black" }}
-                    > */}
-                    {specie.plant_genera.genus_name + " " + specie.species_name}
-                    {/* </Link> */}
-                  </Box>
-                ))}
+                <Box sx={{ fontWeight: "bold" }}> Examples </Box>
                 <Box>
-                  {/* {species.plant_genera.genus_name} {species.species_name} */}
+                  {" "}
+                  {uniqueSpecies.map((specie, index) => (
+                    <Box
+                      key={index}
+                      sx={{
+                        backgroundColor:
+                          index === 0 && selectedIndex !== 0
+                            ? "inherit"
+                            : selectedIndex === index
+                            ? "gray"
+                            : "inherit",
+                        color: selectedIndex === index ? "white" : "black",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => handleItemClick(specie, index)}
+                      onDoubleClick={() => handleDoubleClick(specie, index)}
+                    >
+                      {specie.plants.plant_genera.genus_name +
+                        " " +
+                        specie.plants.species_name}
+                    </Box>
+                  ))}
+                  <Box>
+                    {/* {species.plant_genera.genus_name} {species.species_name} */}
+                  </Box>
                 </Box>
               </Box>
             </Box>
-          </Box>
-        )}
-      </Container>
-    );
+          )}
+        </Container>
+      );
+    }
   }
 }
