930eae6b176c7838950f1ffb511cbd4ccdbfcd85
bonfaceonyango <bonfaceb21@gmail.com>
2024-02-27 21:29:50 +0300

    updated navab on hoover becomes white

--- src/components/navbarMUI.js
+++ src/components/navbarMUI.js
@@ -77,7 +77,7 @@ const useStyles = makeStyles((theme) => ({
     textTransform: "uppercase",
     marginLeft: theme.spacing(15),
     "&:hover": {
-      color: "red",
+      color: "white",
       borderBottom: "1px solid white",
     },
     [theme.breakpoints.down("sm")]: {
