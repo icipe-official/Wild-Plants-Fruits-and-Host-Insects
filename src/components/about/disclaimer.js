a2239f2db7932bcb4600383f682cf173cafbbad9
bonfaceonyango <bonfaceb21@gmail.com>
2024-02-28 14:55:39 +0300

    updated disclaimer

--- src/components/about/disclaimer.js
+++ src/components/about/disclaimer.js
@@ -14,8 +14,10 @@ export default function Disclaimer() {
         The barcodes that have been used for constructing the phylogeny tree of
         respective biocollections were obtained from the BOLD database with
         assumption that taxonomically identified individuals share identical
-        barcodes with the species that have been barcoded. The link to the
-        barcodes are provided upon clicking each species in the phylogeny tree
+        barcodes with the species that have been barcoded. The link to BOLD
+        databse as well as NCBI links of the barcodes are can be accessed upon
+        right clicking the species in the phylogeny tree that brings a n option
+        with links to BOLD database and/or NCBI.
       </Box>
       {/* <Divider /> */}
     </Container>
