b464b3510f7278677ee536ae77b22410d6965c4d
rogonion <declan.obu@outlook.com>
2024-04-05 14:39:09 +0300

    update local container script

--- build-container-local.sh
+++ build-container-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t icipe/wpfhi/app:latest . \
+docker build -t icipe/wpfhi/app_local:latest . \
 --build-arg NEXT_PUBLIC_API_BASE_URL="http://localhost:3000/wpfhi" \
 --build-arg DATABASE_URL="postgresql://postgres:postgres2023@host.docker.internal:5432/wpfhi" \
 --build-arg NEXT_PUBLIC_BASE_PATH="/wpfhi" \
